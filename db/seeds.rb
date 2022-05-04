8dfb947c37f41ec7e7d0a61497a8c3e46805d7c5
Kevin Redmond <kred1150@gmail.com>
2022-05-03 19:56:42 -0500

    Populated the initial database with Products via the seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+product = Product.new(name: "Garbage Bags", price: 13, image_url: "https://m.media-amazon.com/images/I/81wzI+BepES._AC_SL1500_.jpg", description: "Hefty Ultra Strong 80pack")
+product.save
+
+product = Product.new(name: "BlenderBottle", price: 10, image_url: "https://m.media-amazon.com/images/I/61Rx91iwTqL._AC_SL1500_.jpg", description: "Black 28oz Blender Bottle Shaker")
+product.save
+
+product = Product.new(name: "Socks", price: 20, image_url: "https://m.media-amazon.com/images/I/81ErhRybsXL._AC_UL320_.jpg", description: "Adidas Black Crew Socks, 6pk")
+product.save
+
+product = Product.new(name: "Coffee Pot", price: 140, image_url: "https://m.media-amazon.com/images/I/71zWWzVvSLL._AC_SL1500_.jpg", description: "Kuerig K-Duo Coffee Maker, Black")
+product.save
+
+product = Product.new(name: "Shoe Rack", price: 60, image_url: "https://m.media-amazon.com/images/I/81pE3vcLT4L._AC_SL1500_.jpg", description: "Kottwca Shoe Rack Metal Shoe Storage Organizer, Black")
+product.save
