1e5793930bf8f88d6623029c4fa6b72cabd1d517
Kevin Redmond <kred1150@gmail.com>
2022-05-08 10:23:14 -0500

    Added a template to the index method of the controller class and structured the template in index.json.jb

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,7 +1,7 @@
 class ProductsController < ApplicationController
   def index
-    products = Product.all
-    render json: products.as_json(methods: [:friendly_created_at, :is_discounted?, :tax, :total])
+    @products = Product.all
+    render template: "products/index"
   end
 
   def show
@@ -21,7 +21,7 @@ class ProductsController < ApplicationController
   end
 
   def update
-    @product = @product.find_by(id: params["id"])
+    @product = Product.find_by(id: params["id"])
     @product.name = params["name"] || @product.name
     @product.price = params["price"] || @product.price
     @product.image_url = params["image_url"] || @product.image_url
