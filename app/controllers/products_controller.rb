1259dcb897b38b32f3853199d995f57359dd526e
Kevin Redmond <kred1150@gmail.com>
2022-05-09 19:31:19 -0500

    Added validation for Name Price and Description to the product model, and added if conditions for happy/sad paths to the controller

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -16,8 +16,11 @@ class ProductsController < ApplicationController
       image_url: params["image_url"],
       description: params["description"],
     )
-    @product.save
-    render template: "products/show"
+    if @product.save
+      render template: "products/show"
+    else
+      render json: { errors: @product.errors.full_messages }, status: 422
+    end
   end
 
   def update
@@ -26,9 +29,12 @@ class ProductsController < ApplicationController
     @product.price = params["price"] || @product.price
     @product.image_url = params["image_url"] || @product.image_url
     @product.description = params["description"] || @product.description
-    @product.save
 
-    render template: "products/show"
+    if @product.save
+      render template: "products/show"
+    else
+      render json: { errors: @product.errors.full_messages }, status: 422
+    end
   end
 
   def destroy
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,9 @@
 class Product < ApplicationRecord
+  validates :name, presence: true
+  validates :price, presence: true
+  validates :price, numericality: { greater_than: 0 }
+  validates :description, length: { in: 10..500 }
+
   def friendly_created_at
     created_at.strftime("%B %e, %Y")
   end
