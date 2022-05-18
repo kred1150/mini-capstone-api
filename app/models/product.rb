f46395e2720a2eaffa521e43285b358c13496e53
Kevin Redmond <kred1150@gmail.com>
2022-05-18 14:55:09 -0500

    Added relationships between the Users, CartedProducts, Products and Orders models

--- app/models/product.rb
+++ app/models/product.rb
@@ -4,11 +4,12 @@ class Product < ApplicationRecord
   validates :price, numericality: { greater_than: 0 }
   validates :description, length: { in: 10..500 }
 
-  has_many :orders
   has_many :images
   belongs_to :supplier
   has_many :category_products
   has_many :categories, through: :category_products
+  has_many :carted_products
+  has_many :orders, through: :carted_products
 
   def friendly_created_at
     created_at.strftime("%B %e, %Y")
