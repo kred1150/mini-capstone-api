88634fb1326b6764455a27062311f815463754e9
Kevin Redmond <kred1150@gmail.com>
2022-06-14 19:32:03 -0500

    Pre Heroku creation commit

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,21 +6,21 @@
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
 
+supplier = Supplier.create({ name: "P&G", email: "procter_gamble@pg.com", phone_number: "(554) 543 - 8092" })
+
+supplier = Supplier.create({ name: "BlenderBottle", email: "cool_bottles@blender.com", phone_number: "(912) 758 - 0183" })
+
+supplier = Supplier.create({ name: "Adidas", email: "three_stripes@adidas.com", phone_number: "(012) 859 - 8510" })
+
 product = Product.create({ name: "Garbage Bags", price: 13, description: "Hefty Ultra Strong 80pack", supplier_id: 1, inventory: 190 })
 
-product = Product.create({ name: "BlenderBottle", price: 10, description: "Black 28oz Blender Bottle Shaker", supplier_id: 2, inventory: 231})
+product = Product.create({ name: "BlenderBottle", price: 10, description: "Black 28oz Blender Bottle Shaker", supplier_id: 2, inventory: 231 })
 
 product = Product.create({ name: "Socks", price: 20, description: "Adidas Black Crew Socks, 6pk", supplier_id: 3, inventory: 123 })
 
 product = Product.create({ name: "Coffee Pot", price: 140, description: "Kuerig K-Duo Coffee Maker, Black", supplier_id: 1, inventory: 43 })
 
 product = Product.create({ name: "Shoe Rack", price: 60, description: "Kottwca Shoe Rack Metal Shoe Storage Organizer, Black", supplier_id: 1, inventory: 88 })
-
-supplier = Supplier.create({ name: "P&G", email: "procter_gamble@pg.com", phone_number: "(554) 543 - 8092" })
-
-supplier = Supplier.create({ name: "BlenderBottle", email: "cool_bottles@blender.com", phone_number: "(912) 758 - 0183" })
-
-supplier = Supplier.create({ name: "Adidas", email: "three_stripes@adidas.com", phone_number: "(012) 859 - 8510" })
 
 image = Image.create({ url: "https://m.media-amazon.com/images/I/81wzI+BepES._AC_SL1500_.jpg", product_id: 1 })
 
